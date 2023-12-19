8441e0aa55f480bccd2a219648054f822f0ead0c
Shikhar Srivastava <109688678+Shikhar-Srivastava-16@users.noreply.github.com>
2023-12-19 23:45:19 +0000

    added functions and more to implement plugboard

--- Enigma.c
+++ Enigma.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+//list to be checked for all plugboard operations
+char plugboardList[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+void configPlugboard() {
+    
+    char change1;
+    char change2;
+
+    printf("Enter the letter to change followed by the letter to be changed to: ");
+    scanf("%c %c", &change1, &change2);
+
+    //change value of index int(var)
+    plugboardList[(int)change1 - 65] = change2;
+    plugboardList[(int)change2 - 65] = change1;     
+}
+
+char implementPlugboard(char1) {
+    return(plugboardList[(int)char1 - 65]);
+}
 
 int main() {
+    configPlugboard();
+    for (int i = 0; i < sizeof(plugboardList); i++) {
+        printf("%c",plugboardList[i]);
+    }
     return 0;  
 }
